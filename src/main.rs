d6faeb03a32df8e8788702d1a64107d8ced7d1f7
Goran Peretin <goran.peretin@gmail.com>
2020-11-22 10:56:46 -0800

    Gets prices from a postgresql database.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+extern crate config;
+
 use std::sync::Arc;
-use chrono::{Datelike, NaiveDate};
+use std::time::Instant;
+use chrono::{Datelike, NaiveDate, NaiveDateTime};
 
 use druid::{AppLauncher, Color, Data, Lens, Rect, Widget, WindowDesc, PlatformError};
 use druid::kurbo::Line;
@@ -8,6 +11,10 @@ use druid::piet::{FontFamily, RenderContext, Text, TextLayout, TextLayoutBuilder
 use druid::widget::{Flex, Label, Padding, SizedBox};
 use druid::widget::prelude::*;
 
+use postgres::{Client, NoTls};
+
+use rust_decimal::prelude::*;
+
 
 const BAR_WIDTH: i32 = 10;
 const BAR_SPACING: i32 = 5;
@@ -15,6 +22,31 @@ const X_AXIS_LABELS_PADDING: f64 = 20.0;
 const Y_TICK_SPACING: f64 = 50.0; // Ticks on y axis every 50 pixels
 const Y_AXIS_LABELS_PADDING: f64 = 40.0;
 static Y_AXIS_TICK_INCREMENTS: &'static [f64] = &[0.1, 0.5, 1.0, 10.0, 100.0];
+
+
+fn get_daily_price(mut client: Client, symbol: &str) -> Vec<Bar> {
+    let mut bars = vec![];
+
+    for row in client.query("
+        SELECT * FROM myschema.adjusted_prices as ap JOIN myschema.instruments as instr
+        ON ap.instrument_id = instr.id
+        WHERE instr.symbol = $1
+        ORDER BY ap.timestamp DESC LIMIT 100
+    ", &[&symbol]).unwrap() {
+
+        // NaiveDate::parse_from_str(row.get(1), "%Y-%m-%d %H:%M:%S").map(|date| {
+        let dt = row.get::<usize, NaiveDateTime>(1);
+        let bar = Bar {
+            date: Arc::new(dt.date()),
+            open: row.get::<usize, Decimal>(5).to_f64().unwrap(),
+            high: row.get::<usize, Decimal>(6).to_f64().unwrap(),
+            low: row.get::<usize, Decimal>(7).to_f64().unwrap(),
+            close: row.get::<usize, Decimal>(4).to_f64().unwrap()
+        };
+        bars.push(bar);
+    }
+    bars
+}
 
 
 #[derive(Clone, Debug, Lens, Data)]
@@ -76,28 +108,42 @@ impl Widget<AppData> for ChartWidget {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &AppData, env: &Env) {
+        let start_time = Instant::now();
+
+        let size = ctx.size();
+        // size is 100x100 because that's what we defined in the layout method()
+
         let mut max_price: f64 = data.chart.first().unwrap().high;
         let mut min_price: f64 = data.chart.first().unwrap().low;
 
+        let mut bars_to_render = vec![];
+
         for bar in data.chart.iter() {
-            // TODO: use min and max functions here https://doc.rust-lang.org/std/cmp/fn.min.html
-            // Nope - Rust can't compare floats.
+            // Let's only plot bars that we can fit into the available screen area
+            if bars_to_render.len() as i32 * (BAR_WIDTH + BAR_SPACING) > (size.width - Y_AXIS_LABELS_PADDING - 2.0 * BAR_SPACING as f64) as i32 {
+                break;
+            }
+
             if bar.high > max_price {
                 max_price = bar.high
             }
             if bar.low < min_price {
                 min_price = bar.low
             }
+            bars_to_render.push(bar);
         }
 
-        let size = ctx.size();
-        // size is 100x100 because that's what we defined in the layout method()
-
-        let scaling: f64 = size.height / (max_price - min_price);
+        let scaling: f64 = (size.height - X_AXIS_LABELS_PADDING) / (max_price - min_price);
 
         // Plot axis
-        let x_axis = Line::new((BAR_SPACING as f64, size.height - X_AXIS_LABELS_PADDING), (size.width - Y_AXIS_LABELS_PADDING, size.height - X_AXIS_LABELS_PADDING));
-        let y_axis = Line::new((size.width - Y_AXIS_LABELS_PADDING, BAR_SPACING as f64), (size.width - Y_AXIS_LABELS_PADDING, size.height - BAR_SPACING as f64));
+        let x_axis = Line::new(
+            (BAR_SPACING as f64, size.height - X_AXIS_LABELS_PADDING),
+            (size.width - Y_AXIS_LABELS_PADDING, size.height - X_AXIS_LABELS_PADDING)
+        );
+        let y_axis = Line::new(
+            (size.width - Y_AXIS_LABELS_PADDING, BAR_SPACING as f64),
+            (size.width - Y_AXIS_LABELS_PADDING, size.height - BAR_SPACING as f64)
+        );
         ctx.stroke(x_axis, &Color::WHITE, 1.0);
         ctx.stroke(y_axis, &Color::WHITE, 1.0);
 
@@ -133,7 +179,10 @@ impl Widget<AppData> for ChartWidget {
                 .text_color(Color::WHITE)
                 .build()
                 .unwrap();
-            ctx.draw_text(&layout, (size.width - Y_AXIS_LABELS_PADDING + 10.0, current_y_tick * scaling - layout.size().height / 2.0));
+            ctx.draw_text(
+                &layout,
+                (size.width - Y_AXIS_LABELS_PADDING + 10.0, current_y_tick * scaling - layout.size().height / 2.0)
+            );
 
             current_y_tick += closest_tick_size;
         }
@@ -141,7 +190,8 @@ impl Widget<AppData> for ChartWidget {
         // Plot candlesticks
         let mut x_position: i32 = BAR_SPACING * 2; // Let's leave some padding to the left
 
-        for bar in data.chart.iter() {
+        bars_to_render.reverse();
+        for bar in bars_to_render {
             // Let's plot the wick first
             let bar_high = (max_price - bar.high) * scaling;
             let bar_low = (max_price - bar.low) * scaling;
@@ -154,17 +204,12 @@ impl Widget<AppData> for ChartWidget {
             let bar_y_top: f64 = (max_price - higher_value) * scaling;
 
             let bar_start = ((x_position - BAR_WIDTH/2) as f64, bar_y_top);
-            println!("bar start {:?}", bar_start);
 
             let lower_value = if bar.close > bar.open { bar.open } else { bar.close };
             let bar_height = ((max_price - lower_value) * scaling) - bar_y_top;
 
-            println!("bar height {:?}", bar_height);
-
             // from_origin_size means it starts at (10,10) and is 100 wide and 100 tall
             let bar_rect = Rect::from_origin_size(bar_start, (BAR_WIDTH as f64, bar_height as f64));
-
-            println!("bar rect {:?}", bar_rect);
 
             let fill_color = if higher_value == bar.close {
                 Color::rgb(0x00, 0xFF, 0x00)
@@ -187,6 +232,8 @@ impl Widget<AppData> for ChartWidget {
 
             x_position += BAR_WIDTH + BAR_SPACING;
         }
+
+        println!("Total render time: {:?} milliseconds", start_time.elapsed().as_millis());
     }
 }
 
@@ -208,14 +255,19 @@ fn build_ui() -> impl Widget<AppData> {
 fn main() -> Result<(), PlatformError> {
     let window = WindowDesc::new(build_ui);
     let launcher = AppLauncher::with_window(window).use_simple_logger();
-    let bars = Arc::new(vec![
-        Bar { date: Arc::new(NaiveDate::from_ymd(2020,12,1)), open: 100.1, high: 100.3, low: 99.0, close: 100.5 },
-        Bar { date: Arc::new(NaiveDate::from_ymd(2020,12,2)), open: 100.6, high: 101.5, low: 100.6, close: 101.1 },
-        Bar { date: Arc::new(NaiveDate::from_ymd(2020,12,3)), open: 102.6, high: 102.7, low: 100.6, close: 101.1 },
-        Bar { date: Arc::new(NaiveDate::from_ymd(2020,12,4)), open: 101.6, high: 101.9, low: 100.6, close: 101.3 },
-        Bar { date: Arc::new(NaiveDate::from_ymd(2020,12,5)), open: 104.1, high: 104.1, low: 100.1, close: 102.8 },
-    ]);
 
+    let mut cfg = config::Config::default();
+    cfg.merge(config::File::with_name("Config")).unwrap();
+
+    // Instantiate the client and get the data
+    let pg_client = Client::connect(
+        format!("host={host} user={user} password={password} dbname={dbname}",
+            host=&cfg.get_str("database.host").unwrap(),
+            user=&cfg.get_str("database.user").unwrap(),
+            password=&cfg.get_str("database.password").unwrap(),
+            dbname=&cfg.get_str("database.dbname").unwrap(),
+        ).as_str(), NoTls).unwrap();
+    let bars = Arc::new(get_daily_price(pg_client, "ES"));
     launcher.launch(AppData { chart: bars })?;
     Ok(())
 }
